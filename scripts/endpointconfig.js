50f1c2c12e3559550ddb99c3eddfe07b10acc23d
Vivek Singh <38683124+vivesing@users.noreply.github.com>
2025-05-23 10:26:22 +0530

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
+  return 'https://publish-p156520-e1658226.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p149891-e1546481.adobeaemcloud.com';
+  return 'https://author-p156520-e1658226.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
